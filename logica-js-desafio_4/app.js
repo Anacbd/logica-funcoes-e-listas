119c2c7993c65eafeafb716b971ba3ca724e3156
Ana Carolina <102333971+Anacbd@users.noreply.github.com>
2026-04-02 14:45:04 -0300

    Add files via upload

---
+++ logica-js-desafio_4/app.js
@@ -0,0 +1,12 @@
+let listaGenerica = [];
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
